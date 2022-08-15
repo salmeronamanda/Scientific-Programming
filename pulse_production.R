2bf361c1777d2b8930aba95217e1b0c36e764656
Amanda Costa Ayres Salmeron <108414748+salmeronamanda@users.noreply.github.com>
2022-08-15 18:58:27 -0300

    commit errado

--- pulse_production.R
+++
@@ -1,6 +0,0 @@
-pulse_production = function(t,x,parms){
-  alphaval = 1 # Decay rate, hrs-1
-  betamax = 5 #Production rate, e.g., nM/hrs
-  dxdt = (t<10)*betamax - alphaval*x
-  return(list(dxdt))
-}
